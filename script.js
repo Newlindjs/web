52652a168be2f077bde29ae41b7e03a6837153f8
Newlindjs <87732456+Newlindjs@users.noreply.github.com>
2021-08-04 19:16:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function displaytime(){
 document.getElementById('demo').innerHTML= Date();
 }
+
+function myFunction(){
+  document.getElementById("dem1").style.fontSize = "25px";
+  document.getElementById("dem1").style.color = "white";
+  document.getElementById("dem1").style.backgroundColor = "green";
+
+}
